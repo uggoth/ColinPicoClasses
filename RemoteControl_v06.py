e8edd636de03a0a23e7a482d085ab267807ee80f
Colin Walls <uggoth>
2022-10-27 15:22:31 +0100

    neopixel added. car mode works

--- RemoteControl_v06.py
+++ RemoteControl_v06.py
@@ -1,4 +1,4 @@
-#  RemoteControl_v01.py
+#  RemoteControl_v06.py
 
 import GPIOPico_v12 as GPIOPico
 import utime
@@ -33,6 +33,7 @@ class StateMachine():
         self.pin_no = pin_no
         self.gpio = GPIOPico.ControlPin(self.pin_no, self.name + '_control')
         self.pin = self.gpio.pin
+        self.value = None
         # print (StateMachine.allocated)
         for state_machine_no in StateMachine.valid_state_machine_nos:
             str_no = str(state_machine_no)
@@ -44,11 +45,14 @@ class StateMachine():
                 self.state_machine_no = state_machine_no
                 self.instance.active(1)
                 break
-    def get(self):
-        if self.instance.rx_fifo():
-            return self.instance.get()
-        else:
-            return None
+    def get_next_blocking(self):
+        self.value =  self.instance.get()
+        return self.value
+    def get_latest(self):
+        self.value = None
+        while self.instance.rx_fifo():
+            self.value = self.instance.get()
+        return self.value
     def close(self):
         if self.valid:
             self.instance.active(0)
@@ -57,21 +61,20 @@ class StateMachine():
             GPIOPico.GPIO.deallocate(self.pin_no)
 
 class RCSwitch():
-    def __init__(self, name, pin_no, intervals, values):  #  sequence of positions
+    def __init__(self, name, state_machine, intervals, values):  #  sequence of positions
         self.name = name
-        self.pin_no = pin_no
         self.intervals = intervals
         self.values = values
-        self.state_machine = StateMachine(self.name + '_sm', 'MEASURE', self.pin_no)
+        self.state_machine = state_machine
         if not self.state_machine.valid:
             print ('**** bad sm', self.state_machine.name)
         self.previous = 'OFF'
     def __str__(self):
-        return self.name + ' Pin:' + str(self.pin_no)
+        return self.name + '_' + self.state_machine.name
     def close(self):
         self.state_machine.close()
     def get(self):
-        self.value = self.state_machine.get()
+        self.value = self.state_machine.get_latest()
         if self.value is not None:
             for i in range(len(self.intervals)):
                 if self.value < self.intervals[i]:
@@ -80,23 +83,28 @@ class RCSwitch():
         return self.previous
 
 class Joystick():
-    def __init__(self, name, pin_no, keys, values):  #  see Interpolator
+    def __init__(self, name, state_machine, interpolator):
         self.name = name
-        self.pin_no = pin_no
-        self.state_machine = StateMachine(self.name + '_sm', 'MEASURE', self.pin_no)
+        self.state_machine = state_machine
         if not self.state_machine.valid:
             print ('**** bad sm', self.state_machine.name)
-        self.interpolator = Interpolator(keys, values)
-        self.previous = 50
+        self.interpolator = interpolator
+        if interpolator is not None:
+            self.previous = self.interpolator.interpolate(75)
+        else:
+            self.previous = 75  #  mid point
     def close(self):
         self.state_machine.close()
     def get(self):
-        value = self.state_machine.get()
-        if value is not None:
-            self.position = self.interpolator.interpolate(value)
-            if self.position != self.previous:
-                self.previous = self.position
-        return self.previous
+        value = self.state_machine.get_latest()
+        if self.interpolator is not None:
+            if value is not None:
+                self.position = self.interpolator.interpolate(value)
+                if self.position != self.previous:
+                    self.previous = self.position
+            return self.previous
+        else:
+            return value
 
 class Interpolator():
     def __init__(self, keys, values):  #  arrays of matching pairs
@@ -105,6 +113,8 @@ class Interpolator():
         self.keys = keys
         self.values = values
     def interpolate(self, in_key):  #  input is integer
+        if in_key is None:
+            return None
         below_ok = False
         above_ok = False
         for i in range(len(self.keys)):
@@ -130,83 +140,72 @@ VALID_MODES = ['TANK','CAR']
 class RemoteControl():
     def __init__(self,
                  name,
-                 mode,
                  left_side,
                  right_side,
                  left_up_down,
                  left_sideways,
-                 right_up_down
+                 right_up_down,
+                 right_sideways,
+                 mode_switch
                  ):
         #  Note: the gpios are GPIO CONTROL objects
         #        the sides implement fwd(speed) and rev(speed)
         self.name = name
-        if mode not in VALID_MODES:
-            raise ValueError("RemoteControl mode " + mode + " not in " + str(VALID_MODES))
-        self.mode = mode
         self.left_side = left_side
         self.right_side = right_side
-
         self.left_up_down = left_up_down
         self.left_sideways = left_sideways
         self.right_up_down = right_up_down
+        self.right_sideways = right_sideways
+        self.mode_switch = mode_switch
+        self.mode = 'OFF'
+        keys = [49, 62, 74, 76, 88, 100]
+        values = [1.0, 1.0, 1.0, 1.0, 0.0, -1.0]
+        self.left_side_interpolator = Interpolator(keys, values)
+        values = [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0]
+        self.right_side_interpolator = Interpolator(keys, values)
 
     def __str__(self):
         out_string = ''
-        out_string += self.name + ', '
-        out_string += self.mode
+        out_string += self.name
         return out_string
 
-    def calculate_speed_tank(self, left_value, right_value):
+    def calculate_speeds_tank(self, left_value, right_value):
         left_speed = int(self.left_up_down.get())
         right_speed = int(self.right_up_down.get())
         return left_speed, right_speed
 
-    def calculate_speed_car(self, left_value, right_value):
-        return 0, 0
-
-    def calculate_vectors(self, angle, speed):
-        # angle: clockwise angle from 0 (straight ahead)
-        # speed: percentage of full speed
-        self.angle = int(angle % 360)
-        self.speed = int(speed % 100)
-        if self.angle < 45:
-            left_speed = self.speed
-            right_speed = - int(self.speed * ((45 - self.angle) / 45))
-        elif self.angle < 90:
-            left_speed = self.speed
-            right_speed = int(self.speed * ((self.angle - 45) / 45))
-        elif self.angle < 135:
-            left_speed = int(self.speed * ((135 - self.angle) / 45))
-            right_speed = self.speed
-        elif self.angle < 180:
-            left_speed = - int(self.speed * ((self.angle - 135) / 45))
-            right_speed = self.speed
-        elif self.angle < 225:
-            left_speed = - self.speed
-            right_speed = int(self.speed * ((225 - self.angle) / 45))
-        elif self.angle < 270:
-            left_speed = - self.speed
-            right_speed = - int(self.speed * ((self.angle - 225) / 45))
-        elif self.angle < 315:
-            left_speed = - int(self.speed * ((315 - self.angle) / 45))
-            right_speed = - self.speed
-        elif self.angle < 360:
-            left_speed = int(self.speed * ((self.angle - 315) / 45))
-            right_speed = - self.speed
-        else:
-            left_speed = 0
-            right_speed = 0
+    def calculate_speeds_car(self, throttle_value, steering_value):
+        if ((throttle_value is None) or (steering_value is None)):
+            return 0,0
+        left_factor = self.left_side_interpolator.interpolate(steering_value)
+        right_factor = self.right_side_interpolator.interpolate(steering_value)
+        raw_speed = self.left_up_down.get()
+        left_speed = int(raw_speed * left_factor)
+        right_speed = int(raw_speed * right_factor)
         return left_speed, right_speed
-    
 
-    def drive(self, left_value, right_value):
-        if self.mode == 'TANK':
-            left_speed, right_speed = self.calculate_speed_tank(left_value, right_value)
-        elif self.mode == 'CAR':
-            left_speed, right_speed = self.calculate_speed_car(left_value, right_value)
+    def set_mode_from_switch(self):
+        if self.mode_switch == None:
+            self.mode = 'TANK'
         else:
-            print ('**** invalid mode')
-        # print (left_value, left_speed, right_value, right_speed)
+            self.mode = self.mode_switch.get()
+
+    def calculate_speeds(self):
+        self.set_mode_from_switch()
+        if self.mode == 'TANK':
+            left_value = self.left_up_down.get()
+            right_value = self.right_up_down.get()
+            return self.calculate_speeds_tank(left_value, right_value)
+        elif self.mode == 'CAR':
+            throttle_value = self.left_up_down.get()
+            steering_value = self.right_sideways.get()
+            return self.calculate_speeds_car(throttle_value, steering_value)
+        else:
+            return 0,0
+
+    def drive(self):
+        left_speed, right_speed = self.calculate_speeds()
         self.left_side.drive(left_speed)
         self.right_side.drive(right_speed)
         return left_speed, right_speed
@@ -221,7 +220,6 @@ class RemoteControl():
         self.right_side.close()
     
 if __name__ == "__main__":
-    print ("RemoteControl_v04.py\n")
 
     if False:
         print ('Testing Interpolator')
@@ -235,58 +233,50 @@ if __name__ == "__main__":
 
     if False:
         joysticks = {'left_up_down':22, 'left_sideways':17, 'right_up_down':16}
-        which_joystick = 'left_up_down'
+        which_joystick = 'left_sideways'
         print ('Testing State Machine', which_joystick)
         sm_1 = StateMachine(which_joystick, 'MEASURE', joysticks[which_joystick])
         previous = 999
         for i in range(10000):
             utime.sleep_ms(1)
-            value = sm_1.get()
+            value = sm_1.get_latest()
             if ((value is not None) and (value != previous)):
                 print (value)
                 previous = value
         sm_1.close()
 
     if False:
-        print ('Testing State Machines')
+        print ('Testing 3 State Machines')
         joysticks = {'left_up_down':22, 'left_sideways':17, 'right_up_down':16}
-        js_1 = 'left_up_down'
-        sm_1 = StateMachine(js_1, 'MEASURE', joysticks[js_1])
+        #  NOTE: Not using channel 1
         js_2 = 'right_up_down'
         sm_2 = StateMachine(js_2, 'MEASURE', joysticks[js_2])
-        previous_1 = 999
+        js_3 = 'left_up_down'
+        sm_3 = StateMachine(js_3, 'MEASURE', joysticks[js_3])
+        js_4 = 'left_sideways'
+        sm_4 = StateMachine(js_4, 'MEASURE', joysticks[js_4])
         previous_2 = 999
+        previous_3 = 999
+        previous_4 = 999
         for i in range(10000):
             utime.sleep_ms(1)
-            value_1 = sm_1.get()
-            if ((value_1 is not None) and (value_1 != previous_1)):
-                print ('1',value_1)
-                previous_1 = value_1
-            value_2 = sm_2.get()
+            value_2 = sm_2.get_latest()
             if ((value_2 is not None) and (value_2 != previous_2)):
                 print ('2',value_2)
                 previous_2 = value_2
-        sm_1.close()
+            value_3 = sm_3.get_latest()
+            if ((value_3 is not None) and (value_3 != previous_3)):
+                print ('3',value_3)
+                previous_3 = value_3
+            value_4 = sm_4.get_latest()
+            if ((value_4 is not None) and (value_4 != previous_4)):
+                print ('4',value_4)
+                previous_4 = value_4
         sm_2.close()
+        sm_3.close()
+        sm_4.close()
 
     if False:
-        joysticks = {'left_up_down':22, 'left_sideways':17, 'right_up_down':16}
-        which_joystick = 'right_up_down'
-        print ('Testing Joystick', which_joystick)
-        pin_no = joysticks[which_joystick]
-        keys = [49, 74, 76, 101]
-        values = [-100.0, 0.0, 0.0, 100.0]
-        js = Joystick('Testing', pin_no, keys, values)
-        previous = 9999
-        for i in range(10000):
-            utime.sleep_ms(1)
-            value = js.get()
-            if ((value is not None) and (value != previous)):
-                print (value)
-                previous = value
-        js.close()
-
-    if True:
         sw5 = RCSwitch('Switch 5', 5, [65,85,105], ['OFF','TANK','CAR'])
         previous = 9999
         for i in range(10000):
@@ -299,3 +289,19 @@ if __name__ == "__main__":
 
     if False:
         dummy = RemoteControl('dummy', 'RUBBISH',None, None, None, None, None)
+
+    if False:
+        name = 'RC Channel 3'
+        code = 'MEASURE'
+        pin_no = 22
+        sm = StateMachine(name, code, pin_no)
+        utime.sleep_ms(900)
+        print (sm.get_latest())
+        utime.sleep_ms(900)
+        print (sm.get_latest())
+        utime.sleep_ms(900)
+        print (sm.get_latest())
+        utime.sleep_ms(900)
+        print (sm.get_latest())
+
+    print ("RemoteControl_v06.py\n")
